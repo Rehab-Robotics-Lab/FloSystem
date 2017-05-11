cf5bbe3f64332361f96023229a1e0b8a2103e869
Michael Sobrepera <mjsobrep@live.com>
2017-05-11 13:39:12 -0400

    finished demo arduino display code

--- teensy/teensy.ino
+++ teensy/teensy.ino
@@ -22,7 +22,7 @@
 #include <Wire.h>
 #include "FloFace.h"
 
-FloFace floFace = FloFace(0x72,0x71,0x70,0,1,3);
+FloFace floFace = FloFace(0x72,0x71,0x70,3,1,3);
 
 void setup()
 {
@@ -64,14 +64,65 @@ void loop()
     //     }
     // }
 
-    // Orientation test... should have one dot in x direc, 2 in y direc
-    bool bob[64] = {1,1,1,0,0,0,0,0,1};
-    bool sue[168] = {1,1,1,0,0,0,0,0,1};
-    for (int i = 0; i < 64; i++)
+    // Orientation test... should have one dot in x direc, 2 in y direc on bottom left
+    // bool bob[64] = {1,1,1,0,0,0,0,0,1};
+    // bool sue[168] = {1,1,1,0,0,0,0,0,1};
+    // for (int i = 0; i < 64; i++)
+    // {
+    //         Serial.print(bob[i]);
+    // }
+    // floFace.DrawLeftEye(bob);
+    // floFace.DrawRightEye(bob);
+    // floFace.DrawMouth(sue);
+
+    // bool small2d[8][8];
+    // small2d[0][0] = 1;
+    // small2d[1][0] = 1;
+    // small2d[2][0] = 1;
+    // small2d[0][1] = 1;
+    // small2d[3][3] = 1;
+
+    bool small2d[8][8] =
     {
-            Serial.print(bob[i]);
-    }
-    floFace.DrawLeftEye(bob);
-    floFace.DrawRightEye(bob);
-    floFace.DrawMouth(sue);
+        {0,0,0,0,0,0,0,0},
+        {0,0,1,1,1,1,0,0},
+        {0,1,1,1,1,1,1,0},
+        {0,1,0,0,1,1,1,0},
+        {0,1,0,0,1,1,1,0},
+        {1,1,1,1,1,1,1,0},
+        {1,0,1,1,1,1,0,0},
+        {1,1,0,0,0,0,0,0}
+    };
+    bool big2d[8][16] = 
+    {
+        {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,1,0,0,0,0,0,0,0,0,1,0,0,0},
+        {1,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0},
+        {1,0,0,0,0,1,1,1,1,1,1,0,0,0,0,0},
+        {1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0}
+    };
+    bool smallzero[8][8] = {0};
+    bool bigzero[8][16] = {0};
+
+    floFace.DrawLeftEye(small2d);
+    floFace.DrawRightEye(small2d);
+    floFace.DrawMouth(big2d);
+    delay(1000);
+    floFace.DrawLeftEye(smallzero);
+    floFace.DrawRightEye(smallzero);
+    floFace.DrawMouth(bigzero);    
+    delay(1000);
+
+    bool happyMouth[168] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0
+, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+    bool eyeLeft[64] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 1, 0, 0, 1, 1, 1, 0, 0, 1, 0, 0, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1,
+1, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+    floFace.DrawLeftEye(eyeLeft);
+    floFace.DrawRightEye(eyeLeft);
+    floFace.DrawMouth(happyMouth);
+    delay(1000);
 }

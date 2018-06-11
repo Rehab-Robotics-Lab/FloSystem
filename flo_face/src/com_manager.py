75419aeaabb7a3f9886798e0e654a42e3ea87670
Michael Sobrepera <mjsobrep@live.com>
2018-06-11 16:32:15 -0400

    added docs to com manager

--- flo_face/src/com_manager.py
+++ flo_face/src/com_manager.py
@@ -9,17 +9,20 @@ class FaceComs(object):
     will send it over the serial communciation line"""
 
     def __init__(self):
+        """Construct node, open serial port, prep to receive commands"""
         rospy.init_node('face_com_manager')
         self.port = rospy.get_param('port', '/dev/ttyACM0')
         self.coms = SerialCom(self.port, self.data_handler)
         self.command_receipt = rospy.Subscriber('face_state',FaceState, self.new_command)
 
     def new_command(self,msg):
+        """Send info from command to microcontroller"""
         self.coms.sendData([0] + bytize(msg.mouth))        
         self.coms.sendData([1] + bytize(msg.left_eye))
         self.coms.sendData([2] + bytize(msg.right_eye))        
 
     def bytize(flat_data):
+        """Turn data into individual bytes"""
         # flat_data = flatten(data)
         data_bytes = [0]*math.ceil(len(flat_data)/8)
         for i in range(len(data_bytes)):
@@ -28,6 +31,7 @@ class FaceComs(object):
         return data_bytes
 
     def data_handler(received, *data):
+        """Receive information from microcontroller and log it"""
         rospy.logdebug("received as ints:")
         rospy.logdebug(data)
         rospy.logdebug("received as string (may be nonsense):")

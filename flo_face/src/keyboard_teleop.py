c35ca54b8aa1e1d1d017aab95b2b2d02fbf6bced
Michael Sobrepera <mjsobrep@live.com>
2018-06-15 16:37:21 -0400

    made skeleton for teleop

---
+++ flo_face/src/keyboard_teleop.py
@@ -0,0 +1,16 @@
+
+
+avialiable_face_commands = ['q','w','e','r','t','a','s','d','f','g','z','x',
+                            'c','v','1','2','3','4','5','6','7','8','9']
+eye_commands = {'i':'up', 'k':'center', 'j':'left', 'l':'right', ',':'down',
+                'u':'up_left','o':'up_right','m':'down_left','.':'down_right'}
+
+available_eye_commands = ['y','h','b','n','7','8','9']
+
+mappings = dict() # mapping from button press to action key tuples
+
+# get options
+
+# set terminal for sing line input
+
+# wait for input

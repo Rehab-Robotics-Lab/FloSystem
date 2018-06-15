0391fd737728a490ac7f9f8768154ddc88a5b621
Michael Sobrepera <mjsobrep@live.com>
2018-06-15 15:39:01 -0400

    a few changes to speeds

--- robot/humanoid/smooth_motion_demo.py
+++ robot/humanoid/smooth_motion_demo.py
@@ -8,18 +8,18 @@ print('imports done')
 ser = serial.Serial('/dev/ttyUSB0',115200)
 print('connection established')
 # ff:05:03:01:fe
-for i in range(150,0,-5):
+for i in range(180,0,-5):
     ser.write(bytearray([0xFF,0x07,0x02,0x02,0x02,0x10+i,0xfe]))
     ret = ser.read(ser.in_waiting)
     print('iteration {}, returned: {}'.format(i,ret))
     # time.sleep(0.1)
     # time.sleep(0.01)
-    time.sleep(0.025)
+    time.sleep(0.1)
 
-for i in range(150):
+for i in range(180):
     ser.write(bytearray([0xFF,0x07,0x02,0x02,0x02,0x10+i,0xfe]))
     ret = ser.read(ser.in_waiting)
     print('iteration {}, returned: {}'.format(i,ret))
     # time.sleep(0.1)
     # time.sleep(0.01)
-    time.sleep(0.001)
+    time.sleep(0.1)

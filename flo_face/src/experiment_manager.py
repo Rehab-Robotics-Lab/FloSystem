a6b889fa829ef6e92cc345da17604c7e115215b4
Michael Sobrepera <mjsobrep@live.com>
2018-12-08 14:30:29 -0500

    fixed ctrl-c action in experiment manager

--- flo_face/src/experiment_manager.py
+++ flo_face/src/experiment_manager.py
@@ -7,6 +7,7 @@ from flo_face.srv import (GetFaceOptions, GetFaceOptionsResponse,
                           SetFace, SetFaceResponse)
 import random
 import pdb
+import signal
 
 static_mode = 'happy'
 
@@ -16,6 +17,8 @@ def change_face(name):
     resp = set_face(name)
     if not resp.success:
         rospy.logerr('couldn\'t get the face changed: {} when using key: {}'.format(resp.info,name))
+    elif resp.success:
+        rospy.loginfo('set face to:\t{}'.format(name))
     return resp.available_eye_directions
 
 def calc_next_face_change():
@@ -46,6 +49,12 @@ if __name__=='__main__':
     tty.setcbreak(sys.stdin.fileno())
     print('ready for input. Press Ctrl-C to exit')
     print('press s to enable static mode, d to enable dynamic mode, f to enable the full cycle mode')
+    def sigint_handler(signum, frame):
+        print('exiting')
+        termios.tcsetattr(sys.stdin, termios.TCSADRAIN, old_attr)
+        sys.exit()
+    signal.signal(signal.SIGINT, sigint_handler)
+
     # wait for input
     while not rospy.is_shutdown():
         if select.select([sys.stdin], [], [], 0)[0] == [sys.stdin]:

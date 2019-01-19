52521bbfe6b61fad7972a21c045b9f55d7b2186b
Michael Sobrepera <mjsobrep@live.com>
2019-01-19 16:22:17 -0500

    cleaned up code to be oop

--- robot/humanoid/read_from_bolide.py
+++ robot/humanoid/read_from_bolide.py
@@ -2,115 +2,188 @@
 
 import serial
 import time
+import datetime
+import pyqtgraph as pg
+from pyqtgraph.Qt import QtCore, QtGui
+import numpy as np
+from matplotlib import cm
+import time
 
-commands = {'pos':0x03,'current':0x06,'torque':0x07}
 
-motors = {'L-shoulder-flex/exten':1, 'L-shoulder-abduct':2, 'L-med-rot':4, 'L-elbow-flex/exten':11}
 
-def read_data(ser,com):
-    # print('new packet for: {}'.format(type))
-    ser.flushInput()
-    ser.write(bytearray([0xFF,0x04,commands[com],0xfe]))
-    ret = ser.read(40)
-    if len(ret) != 40:
-        # print('wrong length returned, got {}, expected {}'.format(len(ret),40))
-        return
-    header = ord(ret[0])
-    if header != 0xFF:
-        print('first byte read did not match header: {}'.format(header))
-        return
-    len_bit = ord(ret[1])
-    if len_bit != 40:
-        print('wrong length bit sent')
-        return
-    command = ord(ret[2])
-    if not command==commands[com]:
-        print('incorrect command type: {}'.format(command))
-        return
-    end = ord(ret[-1])
-    if end!=0xFE:
-        print('bad end bit')
-        return
-    data = ret[3:-1]
-    final_joint_pos = [0]*18
-    for i in range(18):
-        final_joint_pos[i] = (ord(data[2*i])<<8) + ord(data[2*i + 1])
-    if com=='current':
-        final_joint_pos = [fjp / 200.0 for fjp in final_joint_pos]
-    print('{}:{}'.format(com,final_joint_pos))
-    return final_joint_pos 
+logging_level = 2
 
-def read_battery_voltage(ser):
-    ser.flushInput()
-    ser.write(bytearray([0xFF,0x04,0x08,0xfe]))
-    ret = ser.read(6)
-    if len(ret) != 6:
-        print('wrong length returned')
-        return
-    header = ord(ret[0])
-    if header != 0xFF:
-        print('first byte read did not match header: {}'.format(header))
-        return
-    len_bit = ord(ret[1])
-    if len_bit != 6:
-        print('wrong length bit sent')
-        return
-    command = ord(ret[2])
-    if not command==0x08:
-        print('incorrect command type: {}'.format(command))
-        return
-    end = ord(ret[-1])
-    if end!=0xFE:
-        print('bad end bit')
-        return
-    data = ret[3:-1]
-    final_joint_pos = [0]*18
-    voltage = ((ord(data[0])<<8) + ord(data[1]))*0.0124
-    print('{}:{}'.format('battery voltage',voltage))
-    return voltage 
+def log(level, message):
+    ''' Print the logged info if the current level is high enough. 
+
+    Args:
+        level: The level of this message: 0 - very unimportant, 1 - unimportant, 
+               2 - marginally important, 3 - important, 4 - very important, 
+               5 - critically important. 
+        message: The message to print
+    '''
+    if level >= logging_level:
+        print('[{}] {}'.format(datetime.datetime.now(), message))
+
+class BolideReader:
+    commands = {'pos':0x03,'current':0x06,'torque':0x07}
+
+    motors = {'L-shoulder-flex/exten':1, 'L-shoulder-abduct':2, 'L-med-rot':4, 
+            'L-elbow-flex/exten':11}
+
+    def __init__(self, ser):
+        ''' create an object to read data from the bolide robot
+
+        Args:
+            ser: The serial object attached to the robot
+        '''
+        self.ser = ser
+        self.servo_vals = np.zeros([0,len(self.motors)])
+        self.times = np.zeros(0)
+        self.start_time = time.time()
+        self.current_time = 0 # elapsed time from start
+
+    def read_data(self, target, tries=5):
+        '''Read data from the bolide robot. Check the header, length, and end. 
+        Read either position, current, or torque
+
+        Args:
+            target: the the targeted thing to read. Options are `pos`, 
+                    `current`, `torque`
+            tries:  The number of serial read attempts before giving up. 
+        '''
+        self.ser.flushInput()
+        self.ser.write(bytearray([0xFF,0x04,self.commands[target],0xfe]))
+        ret = ''
+        tries = 5
+        while len(ret) < 1 and tries > 0:
+            ret = self.ser.read(40)
+            tries -= 1
+        if len(ret)>0:
+            header = ord(ret[0])
+        else:
+            log(3,'not enough data returned after tries')
+            return
+        if header != 0xFF:
+            log(3,'first byte read did not match header: {}'.format(header))
+            return
+        else:
+            good_header = True
+        while len(ret) < 40 and tries > 0:
+            ret = ret + self.ser.read(40-len(ret))
+            tries -= 1
+        
+        if len(ret) != 40:
+            log(3,'wrong length returned, got {}, expected {}'.format(
+                len(ret),40))
+            return
+        
+        len_bit = ord(ret[1])
+        if len_bit != 40:
+            log(3,'wrong length bit sent')
+            return
+        command = ord(ret[2])
+        if not command==self.commands[target]:
+            log(3,'incorrect command type: {}'.format(command))
+            return
+        end = ord(ret[-1])
+        if end!=0xFE:
+            log(3,'bad end bit')
+            return
+        data = ret[3:-1]
+        final_joint_pos = [0]*18
+        for i in range(18):
+            final_joint_pos[i] = (ord(data[2*i])<<8) + ord(data[2*i + 1])
+        if target=='current':
+            final_joint_pos = [fjp / 200.0 for fjp in final_joint_pos]
+        # print('{}:{}'.format(com,final_joint_pos))
+        return final_joint_pos 
+
+    def read_battery_voltage(self):
+        ''' Read battery voltage, not ready for usage
+        '''
+        self.ser.flushInput()
+        self.ser.write(bytearray([0xFF,0x04,0x08,0xfe]))
+        ret = self.ser.read(6)
+        if len(ret) != 6:
+            print('wrong length returned')
+            return
+        header = ord(ret[0])
+        if header != 0xFF:
+            print('first byte read did not match header: {}'.format(header))
+            return
+        len_bit = ord(ret[1])
+        if len_bit != 6:
+            print('wrong length bit sent')
+            return
+        command = ord(ret[2])
+        if not command==0x08:
+            print('incorrect command type: {}'.format(command))
+            return
+        end = ord(ret[-1])
+        if end!=0xFE:
+            print('bad end bit')
+            return
+        data = ret[3:-1]
+        final_joint_pos = [0]*18
+        voltage = ((ord(data[0])<<8) + ord(data[1]))*0.0124
+        print('{}:{}'.format('battery voltage',voltage))
+        return voltage 
+
+    def setup_pos_plots(self):
+        '''Set up the pyqt graph to visualize positions'''
+        colormap = cm.get_cmap("viridis")  # cm.get_cmap("CMRmap")
+        colormap._init()
+        # Convert matplotlib colormap from 0-1 to 0 -255 for Qt
+        color_lut = (colormap._lut * 255).view(np.ndarray)  
+
+        self.pos_win = pg.GraphicsWindow()
+        self.pos_win.setWindowTitle('Robot Position')
+
+        p2 = self.pos_win.addPlot()
+        p2.setDownsampling(mode='peak')
+        p2.setClipToView(True)
+        p2.addLegend()
+        self.curves = [p2.plot(pen=pg.mkPen(
+            color_lut[idx*color_lut.shape[0]/len(self.motors)]), 
+            name=name) for idx,name in enumerate(self.motors.keys())]
+
+    def collect_pos_data(self, to_store=-1):
+        '''Ask the robot for its position and store the result
+        
+        Args:
+            to_store: The number of data points to store. If less than zero, 
+                      will store all data points
+        '''
+        position = self.read_data('pos')
+        if position:
+            self.servo_vals = np.append(self.servo_vals,np.atleast_2d(
+                np.asarray(position)[self.motors.values()]),axis=0)
+            self.current_time = time.time()- self.start_time
+            self.times = np.append(self.times, self.current_time)
+            if to_store > 0:
+                self.times = self.times[-to_store:]
+                self.servo_vals = self.servo_vals[-to_store:,:]
+
+    def update_pos_plot(self, time_to_plot=15):
+        '''updates the position plots
+
+        Args:
+            time_to_plot: The amount of time back to plot in seconds
+        '''
+        if len(self.times)>0:
+            plot_min_idx = np.argmax(self.times>self.current_time-15)
+            for idx, curve in enumerate(self.curves):
+                curve.setData(self.times[plot_min_idx:],self.servo_vals[plot_min_idx:,idx])
+            QtGui.QApplication.processEvents()
+            self.pos_win.repaint()
 
 if __name__ == "__main__":
-    import pyqtgraph as pg
-    from pyqtgraph.Qt import QtCore, QtGui
-    import numpy as np
-    from matplotlib import cm
-
-    colormap = cm.get_cmap("viridis")  # cm.get_cmap("CMRmap")
-    colormap._init()
-    color_lut = (colormap._lut * 255).view(np.ndarray)  # Convert matplotlib colormap from 0-1 to 0 -255 for Qt
-
-
-    print('imports done')
     ser = serial.Serial('/dev/ttyUSB0',115200,timeout=0.2)
     print('connection established')
 
-
-    win = pg.GraphicsWindow()
-    win.setWindowTitle('Robot Position')
-
-    servo_vals = np.zeros([1,len(motors)])
-    p2 = win.addPlot()
-    p2.setDownsampling(mode='peak')
-    p2.setClipToView(True)
-    p2.addLegend()
-    curves = [p2.plot(pen=pg.mkPen(color_lut[idx*color_lut.shape[0]/len(motors)]), name=name) for idx,name in enumerate(motors.keys())]
-    # curve2 = p2.plot()
-
+    robot = BolideReader(ser)
+    robot.setup_pos_plots()
     while True:
-        position = read_data(ser,'pos')
-        # bat = read_battery_voltage(ser)
-        # current = read_data(ser,'current')
-        # torque = read_data(ser,'torque')
-        # print('---------------------')
-        if position:
-            print('pos: {}'.format(position))
-            servo_vals = np.append(servo_vals,np.atleast_2d(np.asarray(position)[motors.values()]),axis=0)
-            # curve2.setData(servo2)
-            for idx, curve in enumerate(curves):
-                curve.setData(servo_vals[-500:,idx])
-            QtGui.QApplication.processEvents()
-            win.repaint()
-# if __name__ == "__main__":
-#     ser = serial.Serial('/dev/ttyUSB0',115200,timeout=0.2)
-#     while True:  
-#         position = read_data(ser,'pos')
+        robot.collect_pos_data()
+        robot.update_pos_plot()

65704a06f3d190ed7cf648bce5c72f123f6cd160
Michael <Michael>
2019-04-04 13:56:54 -0400

    cleaned up python code

--- robot/humanoid/flo_humanoid/src/robot_controller.py
+++ robot/humanoid/flo_humanoid/src/robot_controller.py
@@ -1,54 +1,57 @@
 #!/usr/bin/env python
 
-import csv
-import rospy
-import rospkg
+from __future__ import division
+from __future__ import print_function
+
+import math
 import os
 import Queue
+import rospy
+import rospkg
 import serial
-import math
 
-from read_from_bolide import BolideReader 
-from std_msgs.msg import Header
 from sensor_msgs.msg import JointState
 from flo_humanoid.msg import JointTarget
+from read_from_bolide import BolideReader
 
-class BolideController:
+class BolideController(object):
     def __init__(self):
         rospy.init_node('robot_manager')
 
-        self.rospack = rospkg.RosPack()
-        
-        self.port = rospy.get_param('robot_port','/dev/bolide')
+        rospack = rospkg.RosPack()
+
+        port = rospy.get_param('robot_port', '/dev/bolide')
         self.ser = None
         try:
-            self.ser = serial.Serial(self.port, 115200, timeout=0.05)
-        except:
+            self.ser = serial.Serial(port, 115200, timeout=0.05)
+        except serial.SerialException:
             rospy.logerr('failed to connect to bolide')
             return
         rospy.loginfo('connected to robot')
 
         self.joint_publisher = rospy.Publisher(
-                'joint_states', JointState, queue_size=1)
+            'joint_states', JointState, queue_size=1)
 
-        self.command_reader = rospy.Subscriber('target_joint_states',JointTarget,self.new_command)
+        self.command_reader = rospy.Subscriber(
+            'target_joint_states', JointTarget, self.new_command)
 
         self.reader = BolideReader(self.ser)
-        #TODO: read in a paramater file describing servo offsets
-        self.package_path = self.rospack.get_path('flo_humanoid')
-        default_config_fn = os.path.join(self.package_path,'config','joints')
-        self.config_fn = rospy.get_param('robot_joint_config',default_config_fn)
+        package_path = rospack.get_path('flo_humanoid')
+        default_config_fn = os.path.join(package_path, 'config', 'joints')
+        config_fn = rospy.get_param('robot_joint_config',
+                                    default_config_fn)
         self.joint_config = dict()
         self.available_motor_ids = []
         self.available_motor_names = []
-        with open(self.config_fn) as cfile:
+        with open(config_fn) as cfile:
             headings = None
             for row in cfile:
                 if not headings:
                     headings = row.split()
                 else:
                     new_data = row.split()
-                    new_data_dict = {key: value for key, value in zip(headings,new_data)}
+                    new_data_dict = {key: value for key, value in
+                                     zip(headings, new_data)}
                     this_address = int(new_data_dict['address'])
                     this_name = new_data_dict['name']
                     self.joint_config[this_address] = new_data_dict
@@ -61,14 +64,17 @@ class BolideController:
         self.read_loop()
 
     def read_loop(self):
+        """If there are motion tasks to do, do those, otherwise, check the
+        robot's pose"""
         while not rospy.is_shutdown():
             if self.tasks.empty():
                 self.get_pose()
             else:
                 return None
             self.rate.sleep()
-                    
+
     def get_pose(self):
+        """Get the pose of the robot and publish it to the joint state"""
         position = self.reader.read_data('pos')
         if not position:
             rospy.logerr('couldn\'t get postion data')
@@ -80,7 +86,9 @@ class BolideController:
             raw_position = position[id]
             # import pdb
             # pdb.set_trace()
-            rad_position = (raw_position - int(self.joint_config[id]['neutral'])) * int(self.joint_config[id]['inversion'])*2*math.pi/1023
+            rad_position = (raw_position - int(
+                self.joint_config[id]['neutral'])) * int(
+                    self.joint_config[id]['inversion'])*2*math.pi/1023
             names.append(self.joint_config[id]['name'])
             positions.append(rad_position)
         new_msg = JointState()
@@ -88,10 +96,14 @@ class BolideController:
         new_msg.position = positions
         new_msg.header.stamp = rospy.Time.now()
         self.joint_publisher.publish(new_msg)
-        #TODO: take position data and turn it into something that can be broadcast
 
     def new_command(self, msg):
+        """Take a new message from the joint command topic and add it to the task queue
+
+        :param msg: The message that is being passed in that we should parse
+        """
         self.tasks.put(msg)
 
+
 if __name__ == "__main__":
-    controller=BolideController()
+    controller = BolideController()

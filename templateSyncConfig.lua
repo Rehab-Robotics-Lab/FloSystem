860b157b23855a1229143c1aefbc6a822e716c3d
Michael Sobrepera <mjsobrep@live.com>
2019-06-12 16:38:45 -0400

    got template for syncing fixed up

--- templateSyncConfig.lua
+++ templateSyncConfig.lua
@@ -1,5 +1,5 @@
 -- This file is a template config file for lsyncd for working with the robot. 
--- You should copy this file and rename it to mySyncConfig, which is already in 
+-- You should copy this file and rename it to mySyncConfig.lua, which is already in 
 -- the gitignore and will therefore be ignored by source control. You can then
 -- edit it for your needs. It can then be run with `lsyncd <this file>`
 

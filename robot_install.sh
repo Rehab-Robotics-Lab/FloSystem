0dccca3d08fdc900f545ff6a0515d5ceabf7a6c4
Michael Sobrepera <mjsobrep@live.com>
2019-07-12 15:22:59 -0400

    added realsense to robot install script

--- robot_install.sh
+++ robot_install.sh
@@ -21,3 +21,21 @@ cd $prior
 #pip3 install -U boto3
 
 source gen_install.sh
+
+## Kobuki udev rules
+rosrun kobuki_ftdi create_udev_rules
+
+## Realsense
+sudo apt-key adv --keyserver keys.gnupg.net --recv-key C8B3A55A6F3EFCDE || sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-key C8B3A55A6F3EFCDE
+sudo add-apt-repository "deb http://realsense-hw-public.s3.amazonaws.com/Debian/apt-repo xenial main" -u
+sudo apt-get install -y librealsense2-dkms
+sudo apt-get install -y librealsense2-utils
+sudo apt-get install -y librealsense2-dev
+sudo apt-get install -y librealsense2-dbg
+
+prior=$(pwd)
+cd ~/catkin_ws/src
+git clone https://github.com/IntelRealSense/realsense-ros.git
+cd realsense-ros/
+git checkout `git tag | sort -V | grep -P "^\d+\.\d+\.\d+" | tail -1`
+cd $prior

d584342fb736f643accac332f2bb18902f4a9974
Michael Sobrepera <mjsobrep@live.com>
2019-07-12 17:39:53 -0400

    added a package for telepresence and got realsense installed on robot

--- robot_install.sh
+++ robot_install.sh
@@ -32,10 +32,18 @@ sudo apt-get install -y librealsense2-dkms
 sudo apt-get install -y librealsense2-utils
 sudo apt-get install -y librealsense2-dev
 sudo apt-get install -y librealsense2-dbg
+sudo apt install -y ros-kinetic-cv-bridge
+sudo apt install -y ros-kinetic-image-transport
+sudo apt install -y ros-kinetic-tf
+sudo apt install -y ros-kinetic-diagnostic-updater
+sudo apt install -y ros-kinetic-ddynamic-reconfigure
 
 prior=$(pwd)
 cd ~/catkin_ws/src
 git clone https://github.com/IntelRealSense/realsense-ros.git
 cd realsense-ros/
 git checkout `git tag | sort -V | grep -P "^\d+\.\d+\.\d+" | tail -1`
+cd ~/catkin_ws
+catkin_make
+catkin_make install
 cd $prior

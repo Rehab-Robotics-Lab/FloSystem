bd6e444bb1a489447e02877a5470f8ae617d3ad8
Michael Sobrepera <mjsobrep@live.com>
2019-08-22 14:52:46 -0400

    put in checks to not constantly re create the viewer and stuff

--- flo_web/web_app/src/components/urdf.jsx
+++ flo_web/web_app/src/components/urdf.jsx
@@ -5,29 +5,32 @@ import * as ROSLIB from 'roslib';
 // Takes a parameter ros, which is the connection to ros
 function URDF({ ros, connected }) {
   const [viewer, setViewer] = useState(null);
+  const [client, setClient] = useState(null);
 
   // We need to wait until the target diff exists
   useEffect(() => {
-  // The view window
+    if (viewer !== null) return;
+    // The view window
     const vw = new ROS3D.Viewer({
       divID: 'urdf',
       width: 400,
       height: 200,
       antialias: true,
-      background: '#fefef', // sets the background color
+      background: '#f2f2f2', // sets the background color
       alpha: 0.3, // background transparency
       cameraPose: { x: 0.15, y: 0.5, z: 0.05 },
     });
 
 
     // A grid
-    vw.addObject(new ROS3D.Grid());
+    // vw.addObject(new ROS3D.Grid({ num_cells: 5, cellSize: 0.1 }));
 
     setViewer(vw);
   }, []);
 
   useEffect(() => {
     if (!connected) return;
+    if (client !== null) return;
     // The connection to move things around, thresholded to prevent too many redraws
     const tfClient = new ROSLIB.TFClient({
       ros,
@@ -37,13 +40,15 @@ function URDF({ ros, connected }) {
     });
 
     // The URDF Loader and drawer
-    const client = new ROS3D.UrdfClient({
+    const clientT = new ROS3D.UrdfClient({
       ros,
       tfClient,
       path: `${process.env.PUBLIC_URL}/mesh_root/`,
       rootObject: viewer.scene,
       // loader: ROS3D.COLLADA_LOADER_2,
     });
+
+    setClient(clientT);
   });
 
   return (

df50a0ee41ee2c2bd07299bb61915d33a937399b
Michael Sobrepera <mjsobrep@live.com>
2019-08-21 21:00:20 -0400

    basic starting point for web page

---
+++ flo_core/web_app/src/App.jsx
@@ -0,0 +1,18 @@
+import React, { useState } from 'react';
+import './App.css';
+import * as ROSLIB from 'roslib';
+import Header from './components/Header.jsx';
+import URDF from './components/urdf.jsx';
+
+function App() {
+  const [ros, setRos] = useState(null);
+  return (
+    <div className="App">
+      Flo control center
+      <Header ros={ros} setRos={setRos} />
+      <URDF ros={ros} />
+    </div>
+  );
+}
+
+export default App;
---
+++ flo_core/web_app/src/components/urdf.jsx
@@ -0,0 +1,51 @@
+import React, { useEffect, useState } from 'react';
+import * as ROS3D from 'ros3d';
+import * as ROSLIB from 'roslib';
+
+// Takes a parameter ros, which is the connection to ros
+function URDF({ ros }) {
+  const [viewer, setViewer] = useState(null);
+
+  // We need to wait until the target diff exists
+  useEffect(() => {
+  // The view window
+    const vw = new ROS3D.Viewer({
+      divID: 'urdf',
+      width: 400,
+      height: 200,
+      antialias: true,
+    });
+
+
+    // A grid
+    vw.addObject(new ROS3D.Grid());
+
+    setViewer(vw);
+  }, []);
+
+  useEffect(() => {
+    if (!ros) return;
+    // The connection to move things around, thresholded to prevent too many redraws
+    const tfClient = new ROSLIB.TFClient({
+      ros,
+      angularThres: 0.01,
+      transThres: 0.01,
+      rate: 10.0,
+    });
+
+    // The URDF Loader and drawer
+    const client = new ROS3D.UrdfClient({
+      ros,
+      tfClient,
+      path: '%PUBLIC_URL%/urdf/',
+      rootObject: viewer.scene,
+      // loader: ROS3D.COLLADA_LOADER_2,
+    });
+  });
+
+  return (
+    <div id="urdf" />
+  );
+}
+
+export default URDF;

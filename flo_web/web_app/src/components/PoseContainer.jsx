10310f517e50ae7e29691769d2f1eeff6198aee7
Michael Sobrepera <mjsobrep@live.com>
2019-09-04 07:40:28 -0400

    added the ability to save poses

--- flo_web/web_app/src/components/PoseContainer.jsx
+++ flo_web/web_app/src/components/PoseContainer.jsx
@@ -10,9 +10,9 @@ const poseProp = PropTypes.shape({
 
 function Pose({ pose, addToMoveList }) {
   return (
-        <button type="button" onClick={() => { addToMoveList(pose); }}>
-        {pose.pose.description}
-        </button>
+    <button type="button" onClick={() => { addToMoveList(pose); }}>
+      {pose.pose.description}
+    </button>
   );
 }
 
@@ -28,6 +28,13 @@ Pose.propTypes = {
 // Takes a parameter ros, which is the connection to ros
 function PoseContainer({ ros, connected, addToMoveList }) {
   const [PosesList, setPosesList] = useState([]);
+  const [showSave, setShowSave] = useState(false);
+  const [saveLR, setSaveLR] = useState('right');
+  const [saveID, setSaveID] = useState(0);
+  const [saveDescription, setSaveDescription] = useState('');
+  const [pose, setPose] = useState(null);
+  const [poseListener, setPoseListener] = useState(null);
+  const [setPoseSrv, setSetPoseSrv] = useState(null);
 
   // get all of the poses
   useEffect(() => {
@@ -48,14 +55,142 @@ function PoseContainer({ ros, connected, addToMoveList }) {
       }
       setPosesList(poses);
     });
-  }, [connected,ros]);
+
+
+    // TODO: Figure out how to clean up pose listener
+    // poseListener.unsubscribe();
+    // setPoseListener(null);
+    const poseListenerT = new ROSLIB.Topic({
+      ros,
+      name: 'joint_states',
+      messageType: 'sensor_msgs/JointState',
+    });
+    poseListenerT.subscribe((msg) => {
+      setPose(msg);
+    });
+    setPoseListener(poseListenerT);
+
+
+    const setPoseSrvT = new ROSLIB.Service({
+      ros,
+      name: '/set_pose',
+      serviceType: 'flo_core/SetPose',
+    });
+    setSetPoseSrv(setPoseSrvT);
+  }, [connected, ros]);
 
 
   return (
-    <div id="poses-container" style={{maxWidth: '150px',backgroundColor:'white',borderRadius:'25px',padding:'10px',margin:'10px'}}>
+    <div
+      id="poses-container"
+      style={{
+        maxWidth: '150px', backgroundColor: 'white', borderRadius: '25px', padding: '10px', margin: '10px',
+      }}
+    >
       <h2>Poses:</h2>
-      <div style={{display:'flex',flexDirection:'column',overflow:'auto',maxHeight:'400px'}}>
-          {
+
+
+      <button type="button" onClick={() => { setShowSave(true); }} disabled={!connected}>Save Pose</button>
+      {showSave
+        && (
+        <div style={{
+          position: 'absolute', bottom: 0, left: 0, right: 0, top: 0, background: 'rgba(0,0,0,.3)', display: 'flex', justifyContent: 'center', alignItems: 'center',
+        }}
+        >
+          <div style={{
+            width: '200px', background: 'white', borderRadius: '10px', minWidth: '500px', position: 'relative', textAlign: 'center', display: 'flex', flexDirection: 'column',
+          }}
+          >
+            <h3>Save a New Pose or Overwrite an Existing One</h3>
+            <label htmlFor="saveLRButton">
+Arm to save:
+              <button id="saveLRButton" type="button" onClick={() => { saveLR === 'right' ? setSaveLR('left') : setSaveLR('right'); }}>{saveLR}</button>
+              {' '}
+            </label>
+            <label htmlFor="savePoseIDSelector">
+Save As:
+              <select
+                id="savePoseIDSelector"
+                onChange={(obj) => {
+                  setSaveID(obj.target.value);
+                  if (saveID > 0) {
+                    const newDesc = obj.target[obj.target.selectedIndex].textContent;
+                    setSaveDescription(newDesc);
+                  }
+                }}
+              >
+                <option value="0">New Pose</option>
+                {
+                PosesList.map((value) => (
+                  <option value={value.id}>{value.pose.description}</option>
+                ))
+                }
+
+              </select>
+            </label>
+            <label htmlFor="savePoseDescription">
+                Description:
+              <input type="text" value={saveDescription} onChange={(obj) => { setSaveDescription(obj.target.value); }} />
+            </label>
+
+            <button
+              type="button"
+              disabled={
+                    !connected || !saveDescription
+}
+              onClick={() => {
+                const jointsOfIterest = [];
+                const cleanNames = [];
+                const pos = [];
+                for (let idx = 0; idx < pose.name.length; idx += 1) {
+                  if (pose.name[idx].includes(saveLR)) {
+                    jointsOfIterest.push(idx);
+                    cleanNames.push(pose.name[idx].slice(saveLR.length + 1));
+                    pos.push(pose.position[idx]);
+                  }
+                }
+                const newPose = {
+                  description: saveDescription,
+                  joint_names: cleanNames,
+                  joint_positions: pos,
+                };
+                const req = new ROSLIB.ServiceRequest({
+                  pose: newPose,
+                  id: saveID,
+                });
+
+                setPoseSrv.callService(req, (res) => {
+                  const targetId = PosesList.findIndex((item) => (item.id === res.id));
+                  const PosesListT = [...PosesList];
+                  if (targetId === -1) {
+                    PosesListT.push({
+                      id: res.id,
+                      pose: newPose,
+                    });
+                  } else { PosesListT[targetId] = { id: res.id, pose: newPose }; }
+                  setPosesList(PosesListT);
+                  setShowSave(false);
+                  setSaveID(0);
+                  setSaveDescription('');
+                });
+
+
+                // ros serve save id
+              }}
+            >
+Save
+            </button>
+            <button type="button" onClick={() => { setShowSave(false); }}>Cancel</button>
+          </div>
+        </div>
+        )}
+
+
+      <div style={{
+        display: 'flex', flexDirection: 'column', overflow: 'auto', maxHeight: '400px',
+      }}
+      >
+        {
                 PosesList.map((value) => (
                   <Pose
                     id={value.id}
@@ -64,7 +199,7 @@ function PoseContainer({ ros, connected, addToMoveList }) {
                   />
                 ))
             }
-        </div>
+      </div>
     </div>
   );
 }

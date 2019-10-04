ef01f5891a4917da41c21cdc46ee736dc5013c67
Michael Sobrepera <mjsobrep@live.com>
2019-10-04 13:04:49 -0400

    formatting chanes on bolide firmware

--- bolide/Bolide_Y-01_Firmware_v2.0.0/Bolide_Y-01_Firmware_v2.0.0.ino
+++ bolide/Bolide_Y-01_Firmware_v2.0.0/Bolide_Y-01_Firmware_v2.0.0.ino
@@ -22,7 +22,7 @@ static int pose_index[max_pose_index];
 sp_trans_t sequence[max_seq_index];// sequence
 
 //========================= Set up =======================================
-void setup() 
+void setup()
 {
     //Configure all basic setting
     Serial.begin(115200);
@@ -34,7 +34,7 @@ void setup()
 }
 //========================= Main =======================================
 void loop()
-{ 
+{
     USB_Task();                          // USB Communcation motion
     vExeMode_MotionEditor();            // play a sequence if it is setup
     // if(Timer_PowerLow.Timer_Task(1000)) {
@@ -42,7 +42,7 @@ void loop()
     // }
     // #TODO: need to check power level:
     //     PWR_Voltage = analogRead(PWRDET_PIN);   // vol*0.0124
-    // if (PWR_Voltage < Power_Voltage_Alarm) 
+    // if (PWR_Voltage < Power_Voltage_Alarm)
 }
 
 //=========================== Function ================================
@@ -50,9 +50,9 @@ void loop()
 // USB Task
 boolean USB_Task(void)
 {
-    unsigned char temp;   
+    unsigned char temp;
     if(Serial.available() < 2) return false;
-    
+
     if(temp = Serial.read() == packet_header) {
             Motion_Editor_Packet_Task();
     }
@@ -71,7 +71,7 @@ void AIM_Task_Setup(void) {
 }
 
 //Configure eye led board pin
-void LED_Setup(void) { 
+void LED_Setup(void) {
     // Eye LED
     pinMode(LED_BLUE_PIN, OUTPUT);
     pinMode(LED_GREEN_PIN, OUTPUT);
@@ -79,14 +79,14 @@ void LED_Setup(void) {
     pinMode(LSA_LED_BLUE_PIN, OUTPUT);
     pinMode(LSA_LED_GREEN_PIN, OUTPUT);
     pinMode(LSA_LED_RED_PIN, OUTPUT);
-    
-    OCR5A = OCR5B = OCR5C = 0; // Turn off 
+
+    OCR5A = OCR5B = OCR5C = 0; // Turn off
 }
 
 // EEPROM Function
 void eeprom_init(void) {
     uint8_t flag = EEPROM.read(EEP_ADDR_FLAG);
-    if(flag != 0x01) 
+    if(flag != 0x01)
     {
         // EEPROM INIT
         Serial.println("EEP INIT");
@@ -129,7 +129,7 @@ void USB_Flush(void) {
     while(Serial.available() > 0) { Serial.read(); };
 }
 //Motion Editor Task
-boolean Motion_Editor_Packet_Task(void) 
+boolean Motion_Editor_Packet_Task(void)
 {
     static int pBuffer[128] = {0};
     static unsigned char pIndex = 0, pLength = 0xFF, pCMD = 0x00;
@@ -146,7 +146,7 @@ boolean Motion_Editor_Packet_Task(void)
     if(_temp == packet_header) goto _packet_start;
     else {return false;}
     _packet_start:
-    pBuffer[header_address] = _temp; pIndex = 1; _reset_timer4(timeout_limit); 
+    pBuffer[header_address] = _temp; pIndex = 1; _reset_timer4(timeout_limit);
     while((_temp = Serial.read()) == -1) {
         if(packet_timeout_status) {Packet_Error_Feedback(0x00); return false;}
     }
@@ -164,11 +164,11 @@ boolean Motion_Editor_Packet_Task(void)
     }
     _packet_finish:
     motion_editor_mode = true;
-    if(pBuffer[pIndex-1] == packet_tail) 
+    if(pBuffer[pIndex-1] == packet_tail)
     {
         //initial motion editor setting, must tell the robot how many motors there are.
         // send packet: [header, size, command (0x01), num motors, tail]
-        if(pCMD == CMD_init_motor) {        
+        if(pCMD == CMD_init_motor) {
             XYZrobot.poseSize = pBuffer[motor_num_address];
             XYZrobot.readPose();
             Packet_Init(pBuffer[motor_num_address]);
@@ -176,7 +176,7 @@ boolean Motion_Editor_Packet_Task(void)
         else if(pCMD == CMD_set_motor) {        //set motor position
             motor_ID = pBuffer[motor_ID_address];
             position_ID = (pBuffer[motor_pos_msb] << 8) + pBuffer[motor_pos_lsb];
-            // playtime = pBuffer[0x06] // time in units of 10ms 
+            // playtime = pBuffer[0x06] // time in units of 10ms
             // SetPositionI_JOG(motor_ID, playtime, position_ID);
             SetPositionI_JOG(motor_ID, 0, position_ID);
             Packet_Set(motor_ID, position_ID);
@@ -210,7 +210,7 @@ boolean Motion_Editor_Packet_Task(void)
         /// Work with loading then playing a sequence of poses:
         //load total pose number, how many unique poses are in the sequence which you will play?
         // Send packet: [header, size, command (0x10), number of sequences, tail]
-        else if(pCMD == CMD_SEQ_load_PoseCnt) { 
+        else if(pCMD == CMD_SEQ_load_PoseCnt) {
             seq_trigger = false; SeqPos = 0;
             seq_pPoseCnt = pBuffer[seq_pose_cnt_address];
             PoseCnt = 0;
@@ -219,7 +219,7 @@ boolean Motion_Editor_Packet_Task(void)
         }
         //load pose in sequence. you should send a command here for eachpose in the sequence
         // Send packet: [header, size, command (0x11), poseID, msb of motor0, lsb of motor0, msb of motor1, lsb of motor1, ... , msb of motorn, lsb of motorn, tail]
-        else if(pCMD == CMD_SEQ_load_Pose) {            
+        else if(pCMD == CMD_SEQ_load_Pose) {
             static int PoseID = 0, _i = 0;
             seq_trigger = false; SeqPos = 0;
             if(SeqProcessCnt == SEQ_Process_load_PoseCnt) {
@@ -388,7 +388,7 @@ void vExeMode_MotionEditor(void)
 {
     if(seq_trigger) {                    // play sequence edited by motion editor
         Motion_Editor_Seq_Play();
-    }  
+    }
 }
 
 void Motion_Editor_Seq_Play(void) {

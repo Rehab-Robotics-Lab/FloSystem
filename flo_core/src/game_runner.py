e2e4ad582eb9d8059dd05e19dc1f9bcb317e8857
Michael Sobrepera <mjsobrep@live.com>
2019-11-23 17:47:48 -0500

    Filled in the simon says game
    now consistent with the database on the robot

--- flo_core/src/game_runner.py
+++ flo_core/src/game_runner.py
@@ -194,11 +194,15 @@ class GameRunner(object):
                 {'speech': 'in simon says, I will tell you something to do and show you how to do it. If I say simon says, you should do it with me. If I do not say simon says, you should not do the action. Watch out, I may try to trick you.'})
             if not new_def.steps:
                 new_def.steps = [
-                    StepDef(type='move', text='touch your head', id=1),
-                    StepDef(type='move', text='touch your stomach', id=2),
-                    StepDef(type='pose_left', text='raise your left arm', id=5),
-                    StepDef(type='pose_right',
-                            text='raise your right arm', id=5)
+                    StepDef(type='move', text='wave', id=2),
+                    StepDef(type='move', text='clap your hands', id=6),
+                    StepDef(type='move', text='disco', id=8),
+                    StepDef(type='pose_left', text='raise your left arm straight out in front', id=2),
+                    StepDef(type='pose_right', text='raise your right arm straight out in front', id=2),
+                    StepDef(type='pose_right', text='raise your right arm straight out to the side', id=3),
+                    StepDef(type='pose_left', text='raise your left arm straight out to the side', id=3),
+                    StepDef(type='pose_left', text='touch the top of your head with your left hand', id=11),
+                    StepDef(type='pose_right', text='touch the top of your head with your right hand', id=11),
                 ]
 
             actions_bag = []

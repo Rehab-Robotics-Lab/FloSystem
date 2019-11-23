89df0c2d83fda86d27c1bd3a9261ff6eb7dbeee8
Michael Sobrepera <mjsobrep@live.com>
2019-11-23 16:34:55 -0500

    made the launcher have a fixed name

--- start_robot.sh
+++ start_robot.sh
@@ -8,7 +8,7 @@ tmux rename-window startup
 tmux send-keys 'roscore' Enter
 
 tmux split-window -t flo
-tmux send-keys 'mon launch flo_core flo_bringup.launch' Enter
+tmux send-keys 'mon launch --name=flo_launcher flo_core flo_bringup.launch' Enter
 
 tmux split-window -t flo -h
 

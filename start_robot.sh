5b720e4f5c05955c78bd18ba46dbb8b0ddfc306c
Michael Sobrepera <mjsobrep@live.com>
2019-11-23 15:43:53 -0500

    added script to start and connect to robot

---
+++ start_robot.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+set -e
+
+ssh -T nuc-admin@flo-nuc << EOSSH
+tmux new-session -d -s flo
+tmux rename-window startup
+tmux send-keys 'roscore' Enter
+
+tmux split-window -t flo
+tmux send-keys 'mon launch flo_core flo_bringup.launch' Enter
+
+tmux split-window -t flo -h
+
+tmux send-keys 'cd ~/catkin_ws/src/LilFloSystem/flo_web/web_app/src/' Enter
+tmux send-keys 'npm run robot' Enter
+
+tmux rotate-window -t flo
+
+tmux new-window -t flo -n audio
+tmux send-keys 'alsamixer' Enter
+tmux split-window -t flo
+tmux send-keys 'pacmd list-sinks|grep index' Enter
+tmux send-keys 'pacmd set-default-sink'
+
+tmux select-window -t flo -n
+EOSSH
+
+ssh nuc-admin@flo-nuc -t tmux attach-session

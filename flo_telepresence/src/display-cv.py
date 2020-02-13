9ccff56a26d88ff4b21531a7dc3136c4c7252a78
Michael Sobrepera <mjsobrep@live.com>
2020-02-13 10:48:27 -0500

    Made a version that uses an opencv based image display in python

---
+++ flo_telepresence/src/display-cv.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+
+import rospy
+import sys
+from sensor_msgs.msg import Image as smImage
+from cv_bridge import CvBridge, CvBridgeError
+import numpy as np
+import cv2
+import Queue
+
+# Screen is 800x480
+
+
+class RobotScreen(object):
+
+    def __init__(self):
+        self.window = cv2.namedWindow('remote_vid', cv2.WINDOW_NORMAL)
+        cv2.setWindowProperty(
+            'remote_vid', cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+
+        self.image_queue = Queue.Queue()
+
+        # cap = cv2.VideoCapture(0)
+        rospy.init_node('robot_screen')
+        rospy.loginfo('Started Robot Screen Node')
+
+        self.bridge = CvBridge()
+        # msg = rospy.wait_for_message("videofile/image_raw", smImage)
+        # self.new_img(msg)
+        rospy.Subscriber('/remote_video', smImage, self.new_img)
+        self.run_display()
+
+    def run_display(self):
+        rate = rospy.Rate(120)
+        img = None
+        while not rospy.is_shutdown():
+            empty = False
+            while not empty:
+                try:
+                    img = self.image_queue.get_nowait()
+                except Queue.Empty:
+                    empty = True
+            if img is not None:
+                cv2.imshow('remote_vid', img)
+                cv2.waitKey(1)
+            rate.sleep()
+
+    def new_img(self, msg):
+        try:
+            cv_image = self.bridge.imgmsg_to_cv2(msg, "bgr8")
+        except CvBridgeError as err:
+            rospy.logerr('error converting message to cvmat: %s', err)
+            return
+        # res_img = cv2.resize(cv_image, (800, 480))
+        self.image_queue.put(cv_image)
+
+
+if __name__ == '__main__':
+    RobotScreen()

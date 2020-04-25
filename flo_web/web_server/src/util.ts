45acc507a10fa478671f987ea7b759060ef47020
Michael Sobrepera <mjsobrep@live.com>
2020-04-25 16:28:34 -0400

    Removed file based logging from webserver
    I am concerned about crashing the server from over logging.
    Need to learn how to do logging well first

--- flo_web/web_server/src/util.ts
+++ flo_web/web_server/src/util.ts
@@ -16,11 +16,11 @@ const logger = winston.createLogger({
         // - Write to all logs with level `info` and below to `combined.log`
         // - Write all logs error (and below) to `error.log`.
         //
-        new winston.transports.File({
-            filename: '~/logs/error.log',
-            level: 'error',
-        }),
-        new winston.transports.File({ filename: '~/logs/combined.log' }),
+        //new winston.transports.File({
+        //filename: '~/logs/error.log',
+        //level: 'error',
+        //}),
+        //new winston.transports.File({ filename: '~/logs/combined.log' }),
         new winston.transports.Console({
             format: winston.format.simple(),
             level: 'error',

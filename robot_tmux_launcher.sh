fc949321a82972ecd53c9b6dbe125e279187576a
Michael Sobrepera <mjsobrep@live.com>
2020-11-11 19:32:47 -0500

    Update recording - compress and rgb infra, service for pausing recording vid, some install and test fixes
    * a rough version of recording
    
    * got resolutions for scaling sorted
    
    * improving install experience
    
    * making recording the default
    
    * use throttled image on web
    
    * higher resolution, use scaled images on web, record 1 min
    
    * set initial reset true for realsense
    
    * got cameras working more reliably
    
    * a working recording format
    
    * using color for tele and ir for recording
    
    * updated recording params for new setup
    
    * fixing a few launch file things
    
    * Fixed versions for realsense
    
    * set recording to use a relative home path
    
    * Set realsense launchers to work for podium
    
    * got podium setup and broadcasting of all streams working
    
    * Got IR in color
    
    * set to install branch with IR RGB from plusone
    
    * added merging in latest tag to install
    
    * Changed to full res and aligning
    
    * Changed to recording aligned and compressed
    
    * Setup to install osrf nodelet manager
    
    * Have a working copy of recording on and off and web images
    
    * rgb for realsense merged in, using dev branch now
    
    * Added all of the new routing for the realsense cameras
    
    * got recording on off working
    
    * pulled out osrf nodelet since not actually using
    
    * ordered files in cmake for core defs
    
    * added install for recording manager
    
    * removed realsense dependency
    
    This prevents installing the wrong version of realsense
    
    * fixed repo for catkin lint
    
    * sorted programs in flo core cmake

--- robot_tmux_launcher.sh
+++ robot_tmux_launcher.sh
@@ -8,7 +8,7 @@ tmux send-keys 'roscore' Enter
 
 tmux split-window -t flo
 #tmux send-keys 'mon launch --name=flo_launcher flo_core flo_bringup.launch' Enter
-tmux send-keys 'roslaunch --wait flo_core flo_bringup.launch' Enter # it is just more stable..
+tmux send-keys 'roslaunch --wait flo_core flo_bringup.launch record:=True' Enter # it is just more stable..
 
 tmux split-window -t flo -h
 tmux send-keys 'htop' Enter
@@ -20,6 +20,14 @@ tmux send-keys 'npm run tsc && pm2 start ./build/app.js && pm2 logs' Enter
 # need to make this use a build version
 
 tmux rotate-window -t flo
+
+tmux split-window -t flo -h
+#tmux send-keys 'mon launch --name=flo_launcher flo_core flo_bringup.launch' Enter
+tmux send-keys 'sleep 20 && roslaunch --wait flo_telepresence realsense-sp-1.launch record:=True' Enter # it is just more stable..
+
+tmux split-window -t flo
+#tmux send-keys 'mon launch --name=flo_launcher flo_core flo_bringup.launch' Enter
+tmux send-keys 'sleep 30 && roslaunch --wait flo_telepresence realsense-sp-2.launch record:=True' Enter # it is just more stable..
 
 tmux new-window -t flo -n audio
 tmux send-keys 'alsamixer' Enter

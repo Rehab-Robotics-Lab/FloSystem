d837acbabc76dff4698795901f1a889aa8a79b93
Michael Sobrepera <mjsobrep@live.com>
2020-11-15 20:36:36 -0500

    Move robot to ubuntu 18 with lots of install improvements (#180)
    * a bunch of changes to make work as we transition to ubuntu 18 on robot
    
    * fixed version of video_stream_opencv
    
    * fixed placement of skip keys command
    
    * fixed how we pull in the rosbridge suite

--- node_install.sh
+++ node_install.sh
@@ -7,3 +7,13 @@ cd ~/Downloads
 curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
 sudo apt-get install -y nodejs
 cd $prior
+
+## install some dependencies
+NPM_CONFIG_PREFIX=~/.npm-global
+
+npm install -g pm2
+
+prior=$(pwd)
+cd ~/catkin_ws/src/LilFloSystem/flo_web/webrtc_robot_pinger
+npm install
+cd $prior
--- robot_install.sh
+++ robot_install.sh
@@ -2,16 +2,61 @@
 
 set -e
 
+sudo apt-get update -y && sudo apt-get upgrade -y
 sudo apt-get install python3-pip curl git -y
 
 ## Adding to the bashrc file
 grep -qxF 'source ~/catkin_ws/src/LilFloSystem/bash_includes' ~/.bashrc || echo 'source ~/catkin_ws/src/LilFloSystem/bash_includes' >> ~/.bashrc
 
-bash aws_install.sh
-
 source gen_install.sh
 
+bash aws_install.sh
+
 ## Kobuki udev rules
-rosrun kobuki_ftdi create_udev_rules
+sudo apt-get install ros-melodic-kobuki-ftdi
+source ~/.bashrc && rosrun kobuki_ftdi create_udev_rules
 
 bash node_install.sh
+
+sudo apt-get install -y tmux
+
+#From: https://github.com/mjsobrep/Turtlebot2-On-Melodic/blob/master/install_basic.sh
+echo "Installing Kobuki"
+prior=$(pwd)
+cd ~/catkin_ws/src
+git clone https://github.com/turtlebot/turtlebot.git
+git clone https://github.com/turtlebot/turtlebot_msgs.git
+git clone https://github.com/turtlebot/turtlebot_apps.git
+git clone https://github.com/turtlebot/turtlebot_simulator.git
+
+git clone https://github.com/yujinrobot/kobuki_msgs.git
+git clone --single-branch --branch melodic https://github.com/yujinrobot/kobuki.git
+mv kobuki/kobuki_description kobuki/kobuki_node \
+  kobuki/kobuki_keyop kobuki/kobuki_safety_controller \
+  kobuki/kobuki_bumper2pc ./
+rm -rf kobuki
+
+git clone --single-branch --branch melodic https://github.com/yujinrobot/kobuki_desktop.git
+mv kobuki_desktop/kobuki_gazebo_plugins ./
+rm -rf kobuki_desktop
+
+git clone https://github.com/yujinrobot/yujin_ocs.git
+mv yujin_ocs/yocs_cmd_vel_mux yujin_ocs/yocs_controllers .
+rm -rf yujin_ocs
+
+sudo apt-get install ros-melodic-kobuki-* -y
+sudo apt-get install ros-melodic-ecl-streams -y
+
+# necessary for build and gazebo
+sudo apt-get install ros-melodic-depthimage-to-laserscan -y
+sudo apt-get install ros-melodic-joy -y
+sudo apt-get install ros-melodic-yocs-velocity-smoother -y
+
+echo "Installing Vid Stream Opencv"
+git clone https://github.com/ros-drivers/video_stream_opencv.git
+
+cd ~/catkin_ws
+rm -rf build devel install
+catkin_make
+catkin_make install
+cd $prior

0acf0106cc9d3e32b805e225e9980a5907dac74e
Michael Sobrepera <mjsobrep@live.com>
2021-03-06 20:53:30 -0500

    updated realsense keys

--- realsense_install.sh
+++ realsense_install.sh
@@ -12,7 +12,7 @@ if(($(cat /etc/os-release | grep VERSION_ID|grep -o '".*"' | sed 's/"//g' | cut 
 fi
 fi
 
-sudo apt-key adv --keyserver keys.gnupg.net --recv-key C8B3A55A6F3EFCDE || sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-key C8B3A55A6F3EFCDE
+sudo apt-key adv --keyserver keys.gnupg.net --recv-key F6E65AC044F831AC80A06380C8B3A55A6F3EFCDE || sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-key F6E65AC044F831AC80A06380C8B3A55A6F3EFCDE
 echo "installing for ros version: ${ROS_VERSION}"
 ## Realsense
 sudo add-apt-repository "deb http://realsense-hw-public.s3.amazonaws.com/Debian/apt-repo ${OS_VERSION} main" -u

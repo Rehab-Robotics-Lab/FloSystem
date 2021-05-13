ee9f2e5c9368871b5af67c9873d0cf5d7c77d9f6
Michael Sobrepera <mjsobrep@live.com>
2021-05-12 22:32:06 -0400

    added upgrade to install

--- gen_install.sh
+++ gen_install.sh
@@ -7,6 +7,7 @@ echo "BEGINING ROS INSTALL"
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
 sudo apt-get -qq update -y
+sudo apt-get -qq upgrade -y
 
 if(($(cat /etc/os-release | grep VERSION_ID|grep -o '".*"' | sed 's/"//g' | cut -c1-2 )==16));then
     ROS_VERSION="kinetic"

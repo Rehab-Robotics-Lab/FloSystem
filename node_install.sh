10bf1e1b4f9fe7a9adffb5b758dcc223a2457e28
Michael Sobrepera <mjsobrep@live.com>
2021-07-23 05:39:20 -0400

    bumped node verson to latest lts

--- node_install.sh
+++ node_install.sh
@@ -4,7 +4,7 @@ set -e
 ## Node
 prior=$(pwd)
 cd ~/Downloads
-curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
+curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
 sudo apt-get install -y nodejs
 cd $prior
 

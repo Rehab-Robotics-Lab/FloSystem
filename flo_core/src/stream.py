cad44104c0b337f53b4ef249d0599bb83807032f
Ryan Comisky <rcomisky@sch.org>
2021-07-23 17:51:56 -0400

    here is he stream assessment with ids for the real robot

--- flo_core/src/stream.py
+++ flo_core/src/stream.py
@@ -7,15 +7,15 @@ def stream(process_step, neutral):
     actions_list.append(
         {'speech': 'Hi I\'m Flo.  For this activity we are going to be doing some actions. If I do an action, please do it with me. I will demonstrate all movements mirrored. Are you ready to get started?'})
     targ, _ = process_step(
-        StepDef(type='pose_both', id=1, time=1))
+        StepDef(type='pose_both', id=6, time=1))
     actions_list.append(
         {'speech': 'first raise your arms above your head', 'targets': targ})
     targ, _ = process_step(
-        StepDef(type='pose_both', id=7, time=1))
+        StepDef(type='pose_both', id=2, time=1))
     actions_list.append(
         {'speech': 'Extend your arms as far as they can go toward the ceiling', 'targets': targ})
     targ, _ = process_step(
-        StepDef(type='pose_both', id=3, time=1))
+        StepDef(type='pose_both', id=2, time=1))
     actions_list.append(
         {'speech': 'shrug your shoulders as high as you can', 'targets': targ})
     targ, _ = process_step(
@@ -23,5 +23,10 @@ def stream(process_step, neutral):
     actions_list.append(
         {'speech': 'Now touch the top of your head with your right hand'})
     targ, _ = process_step(
-        StepDef(type='pose_both', id=1, time=1))
+        StepDef(type='pose_both', id=4, time=1))
+    actions_list.append(
+        {'speech': 'Now reach forward towards me'})
+    targ, _ = process_step(
+        StepDef(type='pose_both', id=3, time=1))
     return actions_list
+    

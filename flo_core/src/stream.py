a1d06125a430f76d6b769f91cab732f06724715d
Ryan Comisky <rcomisky@sch.org>
2021-07-26 12:26:45 -0400

    made some text changes to make more sense with the code

--- flo_core/src/stream.py
+++ flo_core/src/stream.py
@@ -26,7 +26,7 @@ def stream(process_step, neutral):
     targ, _ = process_step(
         StepDef(type='pose_both', id=2, time=1))
     actions_list.append(
-        {'speech': 'Extend your arms as far as they can go toward the ceiling', 'targets': targ})
+        {'speech': 'Extend your arms as far as you can toward the ceiling', 'targets': targ})
     targ, _ = process_step(
         StepDef(type='pose_both', id=2, time=1))
     actions_list.append(
@@ -34,11 +34,15 @@ def stream(process_step, neutral):
     targ, _ = process_step(
         StepDef(type='pose_both', id=13, time=1))
     actions_list.append(
-        {'speech': 'Now touch the top of your head with your right hand'})
+        {'speech': 'Now touch the top of your head with both hands'})
     targ, _ = process_step(
         StepDef(type='pose_both', id=4, time=1))
     actions_list.append(
         {'speech': 'Now reach forward towards me'})
     targ, _ = process_step(
         StepDef(type='pose_both', id=3, time=1))
+    actions_list.append(
+        {'speech': 'Thanks for playing with me'})
+    targ, _ = process_step(
+        StepDef(type='pose_both', id=12, time=1))
     return actions_list

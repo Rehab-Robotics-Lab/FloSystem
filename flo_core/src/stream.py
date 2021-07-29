e31d45a3879b7f6b34caf1251d1d2a2d61222a42
Ryan Comisky <rcomisky@sch.org>
2021-07-29 14:05:32 -0400

    added targets to make the last 2 args to make it work

--- flo_core/src/stream.py
+++ flo_core/src/stream.py
@@ -20,29 +20,29 @@ def stream(process_step, neutral):
                    'If I do an action, please do it with me. I will demonstrate all the ' +
                    'actions before you try them. Are you ready to get started?'})
     targ, _ = process_step(
-        StepDef(type='pose_both', id=1, time=1))
+        StepDef(type='pose_both', id=5, time=1))
     actions_list.append(
         {'speech': 'first raise your arms above your head', 'targets': targ})
-    targ, _ = process_step(
+    targ, _ = process_step
         StepDef(type='pose_both', id=5, time=1))
     actions_list.append(
         {'speech': 'Extend your arms as far as you can toward the ceiling', 'targets': targ})
     targ, _ = process_step(
-        StepDef(type='pose_both', id=5, time=1))
-    actions_list.append(
-        {'speech': 'shrug your shoulders as high as you can', 'targets': targ})
-    targ, _ = process_step(
         StepDef(type='pose_both', id=1, time=1))
-    actions_list.append(
-        {'speech': 'Now touch the top of your head with both hands'})
+    actions_list.append(t
+        {'speech': 'shrug your shoulders as high as you can', 'targets': targ})
     targ, _ = process_step(
         StepDef(type='pose_both', id=9, time=1))
     actions_list.append(
-        {'speech': 'Finally reach forward towards me with both arms'})
+        {'speech': 'Now touch the top of your head with both hands', 'targets' : targ})
     targ, _ = process_step(
         StepDef(type='pose_both', id=7, time=1))
     actions_list.append(
-        {'speech': 'Thanks for playing with me'})
+        {'speech': 'Finally reach forward towards me with both arms', 'target' : targ})s 
+    targ, _ = process_step(
+        StepDef(type='pose_both', id=1, time=1)),
+    actions_list.a
+        {'speech': 'Thanks for playing with me', 'target' : targ})
     targ, _ = process_step(
         StepDef(type='pose_both', id=22, time=1))
     return actions_list

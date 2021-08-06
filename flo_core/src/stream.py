c3a0838c8f68d99f306ac40584852b2e915e2c6a
Michael Sobrepera <mjsobrep@live.com>
2021-08-06 18:50:41 -0400

    style errors fixed in stream.py

--- flo_core/src/stream.py
+++ flo_core/src/stream.py
@@ -26,7 +26,7 @@ def stream(process_step, neutral):
     targ, _ = process_step(
         StepDef(type='pose_both', id=5, time=1))
     actions_list.append(
-        {'speech': 'Extend your arms as far as you can toward the ceiling', 'targets' : targ})
+        {'speech': 'Extend your arms as far as you can toward the ceiling', 'targets': targ})
     targ, _ = process_step(
         StepDef(type='pose_both', id=1, time=1))
     actions_list.append(
@@ -34,15 +34,15 @@ def stream(process_step, neutral):
     targ, _ = process_step(
         StepDef(type='pose_both', id=9, time=1))
     actions_list.append(
-        {'speech': 'Now touch the top of your head with both hands', 'targets' : targ})
+        {'speech': 'Now touch the top of your head with both hands', 'targets': targ})
     targ, _ = process_step(
         StepDef(type='pose_both', id=7, time=1))
     actions_list.append(
-        {'speech': 'Finally reach forward towards me with both arms', 'target' : targ}) 
+        {'speech': 'Finally reach forward towards me with both arms', 'target': targ})
     targ, _ = process_step(
         StepDef(type='pose_both', id=1, time=1))
     actions_list.append(
-        {'speech': 'Thanks for playing with me', 'target' : targ})
+        {'speech': 'Thanks for playing with me', 'target': targ})
     targ, _ = process_step(
         StepDef(type='pose_both', id=22, time=1))
     return actions_list
